25f25393cba597d50c4a51582c33c3eff54795f6
yusuf.kesgin <yusuf.kesgin@aile.gov.tr>
2022-06-23 14:50:48 +0300

    Completing assignment

--- src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Ingredient.java
@@ -9,8 +9,8 @@ public class Ingredient {
     private Long id;
     private String description;
     private BigDecimal amount;
-
-    //private UnitOfMeasure uom;
+    @OneToOne(fetch = FetchType.EAGER)//OneToOne default is eager
+    private UnitOfMeasure uom;
     @ManyToOne
     private Recipe recipe;
 
@@ -44,5 +44,13 @@ public class Ingredient {
 
     public void setRecipe(Recipe recipe) {
         this.recipe = recipe;
+    }
+
+    public UnitOfMeasure getUom() {
+        return uom;
+    }
+
+    public void setUom(UnitOfMeasure uom) {
+        this.uom = uom;
     }
 }
---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/UnitOfMeasure.java
@@ -0,0 +1,11 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+
+@Entity
+public class UnitOfMeasure {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String uom;
+}
