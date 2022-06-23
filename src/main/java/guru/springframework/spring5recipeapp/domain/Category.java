3206a8efe5829146df71e6d5cb52ed5ee65380b1
yusuf.kesgin <yusuf.kesgin@aile.gov.tr>
2022-06-23 17:11:40 +0300

    Adding Many to Many relation to Recipe and Category

---
+++ src/main/java/guru/springframework/spring5recipeapp/domain/Category.java
@@ -0,0 +1,13 @@
+package guru.springframework.spring5recipeapp.domain;
+
+import javax.persistence.*;
+import java.util.Set;
+@Entity
+public class Category {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String description;
+    @ManyToMany(mappedBy = "categories")
+    private Set<Recipe> recipes;
+}
