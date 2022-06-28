f4e2bbb80b3cf64db48b06bf925bd62539faeb69
yusuf.kesgin <yusuf.kesgin@aile.gov.tr>
2022-06-28 09:41:44 +0300

    Refactoring project lombok and Slf4j

--- src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
+++ src/main/java/guru/springframework/spring5recipeapp/controllers/IndexController.java
@@ -5,12 +5,13 @@ import guru.springframework.spring5recipeapp.domain.UnitOfMeasure;
 import guru.springframework.spring5recipeapp.repositories.CategoryRepository;
 import guru.springframework.spring5recipeapp.repositories.UnitOfMeasureRepository;
 import guru.springframework.spring5recipeapp.service.RecipeService;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import java.util.Optional;
-
+@Slf4j
 @Controller
 public class IndexController {
     private final RecipeService recipeService;
@@ -21,6 +22,7 @@ public class IndexController {
 
     @RequestMapping({"", "/", "/index"})
     public String getIndexPage(Model model) {
+        log.debug("I'm in the getIndexPage");
         model.addAttribute("recipes", recipeService.getRecipes());
         return "index";
     }
