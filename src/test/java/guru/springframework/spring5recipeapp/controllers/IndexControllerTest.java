ed04348731f3ceafd0564dab99e2e527b4328651
yusuf.kesgin <yusuf.kesgin@aile.gov.tr>
2022-06-29 09:15:13 +0300

    Added argument captur in unit test for controller

--- src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
+++ src/test/java/guru/springframework/spring5recipeapp/controllers/IndexControllerTest.java
@@ -5,6 +5,7 @@ import guru.springframework.spring5recipeapp.service.RecipeService;
 import guru.springframework.spring5recipeapp.service.RecipeServiceImpl;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.mockito.ArgumentCaptor;
 import org.mockito.Mock;
 import org.mockito.MockitoAnnotations;
 import org.springframework.ui.Model;
@@ -40,9 +41,25 @@ class IndexControllerTest {
         assertEquals(recipes.size(), 1);
 
 **/
+        //given
+        Set<Recipe> recipes = new HashSet<>();
+        recipes.add(new Recipe());
+        Recipe recipe = new Recipe();
+        recipe.setId(1L);
+        recipes.add(recipe);
+
+        when(recipeService.getRecipes()).thenReturn(recipes);
+
+        ArgumentCaptor<Set<Recipe>> argumentCaptor = ArgumentCaptor.forClass(Set.class);
+
+        //when
         String viewName = indexController.getIndexPage(model);
+
+        //then
         assertEquals("index", viewName);
         verify(recipeService, times(1)).getRecipes();
-        verify(model, times(1)).addAttribute(eq("recipes"), anySet());
+        verify(model, times(1)).addAttribute(eq("recipes"), argumentCaptor.capture());
+        Set<Recipe> setInController = argumentCaptor.getValue();
+        assertEquals(2, setInController.size());
     }
 }
